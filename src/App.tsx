0b4019fc9190003f746b82cc8306e36c7ca64d9b
Andrew <andrewurom@gmail.com>
2021-02-09 04:30:38 +0100

    Added Hover and Neumorphism

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import './App.css';
 import Button from './components/Custom-Button/Custom-Button.component';
 import InputField from './components/Custom-Inputs/Custom-Input.component';
+import Hover from './components/Hover-Component/Hover.component';
+import Neumorphism from './components/Neumorphism-Animation/Neumorphism.component';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <InputField size="small" type="password" color="blue" padding="2em" placeholder="Small" shape="oblong" width="20%" />
-      <Button name="shop now" color="#2f2a2a" backgroundcolor = "#ffffff" width = "200px" border= "block"  height= "50px"/>
+      <Button name="shop now" color="#2f2a2a" backgroundcolor="#ffffff" width="200px" border="block" height="50px" />
+      <Hover name="Akius" />
+      
+      <Neumorphism/>
     </div>
   );
 }
